b9e41ca4f1d4441cf5b8fb9858feef71fdbbee48
wanghao <1613104610@qq.com>
2024-01-15 10:33:36 +0800

    Update card body content

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,7 +6,7 @@
           <span>card title</span>
         </div>
       </template>
-      card body
+      card body content
     </el-card>
     <div h-100px w-full rounded-2 bg-red />
     <div h-1000px w-full rounded-2 bg-blue />
