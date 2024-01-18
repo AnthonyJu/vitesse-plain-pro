9bd30f23525f685ee828f7d930b3738fd5680de8
wanghao <1613104610@qq.com>
2024-01-18 18:32:57 +0800

    Add Svg component and route

---
+++ src/pages/excellent-components/svg-com.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-container">
+    Svg-icon
+    <SvgIcon name="iconfont icon-shuju1" color="red" :size="30" />
+  </div>
+</template>
+
+<route lang="yaml">
+  meta:
+    name: Svg-icon
+</route>
+
+<script setup lang='ts'>
+</script>
