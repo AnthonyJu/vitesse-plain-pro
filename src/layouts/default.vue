61e61be759f0b5cf3d762810ea9222b5d1b2a481
jupeng <1662345805@qq.com>
2024-01-18 09:31:08 +0800

    Remove unused code and update scrollbar behavior

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -29,25 +29,41 @@
 
       <!-- 内容 -->
       <main mr-16px mt-16px flex-1 overflow-hidden rounded-10px>
-        <Scrollbar>
-          <router-view v-slot="{ Component }">
-            <transition name="opacity" mode="out-in">
+        <ElScrollbar ref="scrollbar" class="custom-scrollbar">
+          <RouterView v-slot="{ Component }">
+            <Transition mode="out-in" name="opacity" @before-enter="handleBeforeEnter">
               <component :is="Component" />
-            </transition>
-          </router-view>
-        </Scrollbar>
+            </Transition>
+          </RouterView>
+        </ElScrollbar>
       </main>
     </el-container>
   </div>
 </template>
 
 <script setup lang='ts'>
+import { ElScrollbar } from 'element-plus'
 import Logo from './components/Logo.vue'
 import Menu from './components/Menu.vue'
 import User from './components/User.vue'
+
+const scrollbar = ref<InstanceType<typeof ElScrollbar>>()
+
+function handleBeforeEnter() {
+  scrollbar.value?.update()
+  scrollbar.value?.scrollTo({ top: 0 })
+}
 </script>
 
 <style lang='scss' scoped>
+.custom-scrollbar {
+  ::v-deep(.el-scrollbar__bar.is-vertical) {
+    position: fixed;
+    top: 95px;
+    right: 4.5px;
+  }
+}
+
 .opacity-enter-active,
 .opacity-leave-active {
   will-change: transform;
