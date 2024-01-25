d3f1ca56b33d495d659d0aa6acd4e6d6b53684b3
wanghao <1613104610@qq.com>
2024-01-25 12:03:10 +0800

    Add shiki package and word-cloud-chart.vue component

--- src/pages/excellent-components/notice-bars.vue
+++ src/pages/excellent-components/notice-bars.vue
@@ -2,19 +2,19 @@
   <div class="main-container">
     <el-card shadow="hover" header="滚动通知栏：默认">
       <NoticeBar
-        text="🎉🎉🔥基于vue3.x、Typescript、vite、Element-plus等，适配手机、平板、pc的后台开源免费模板库（vue2.x请切换vue-prev-admin分支），仓库地址：https://gitee.com/lyt-top/vue-next-admin"
+        text="😎孩儿立志出乡关,👨‍💻学不成名誓不还。🌳埋骨何须桑梓地,🏕人生无处不青山。"
       />
     </el-card>
 
     <el-card shadow="hover" header="滚动通知栏：设置样式" class="mt-15px">
       <NoticeBar
-        text="🎉🎉🔥基于vue3.x、Typescript、vite、Element-plus等，适配手机、平板、pc的后台开源免费模板库（vue2.x请切换vue-prev-admin分支），仓库地址：https://gitee.com/lyt-top/vue-next-admin"
+        :text="state.textStr"
         left-icon="carbon:user-speaker"
         right-icon="carbon:chevron-right"
         background="#ecf5ff"
         mode="link"
         color="#409eff"
-        :speed="200"
+        :speed="100"
         @link="linkFn"
       />
     </el-card>
@@ -66,11 +66,12 @@
 <script setup lang='ts'>
 // 定义变量内容
 const state = reactive({
+  textStr: '🎼我肯定在几百年前就说过爱你👩‍💼只是你忘了我也没记起🚶‍♀️走过 路过 没遇过🚶‍♂️回头 转头 还是错🎈你我不曾感受过 相撞在街口🎶相撞在街口...',
   noticeList: [
-    '🎉🎉🔥基于vue3.x 、Typescript、vite、Element plus等',
-    '适配手机、平板、pc的后台开源免费模板库（vue2.x请切换vue-prev-admin分支）',
-    '仓库地址：https://gitee.com/lyt-top/vue-next-admin',
-    '演示地址：https://lyt-top.gitee.io/vue-next-admin-preview/#/login',
+    '如果有一天我变得很有钱 我会买下所有难得一见的笑脸',
+    '让所有可怜的孩子不再胆怯 所有邪恶的人不再掌握话语权',
+    '如果有一天我变得很有钱 我会想尽一切办法倒流时间',
+    '不是为了人类理想做贡献 只是想和她说一句我很抱歉',
   ],
   tableData: [
     {
