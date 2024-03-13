777ff1fba3dc752b078043c31ab5bf7d282b0e8e
jupeng <1662345805@qq.com>
2024-03-13 12:46:31 +0800

    Remove web-font-download plugin and update auto-imports

--- plugins/index.ts
+++ plugins/index.ts
@@ -2,26 +2,24 @@ import { autoImports } from './auto-imports'
 import { components } from './components'
 import { elementPlus } from './element-plus'
 import { layouts } from './layouts'
+import { supportSetupName } from './vue-support-setup-name'
 import { unocss } from './unocss'
 import { vitePWA } from './vite-pwa'
 import { vueDevTools } from './vue-devtools'
 import { vueI18n } from './vue-i18n'
 import { vueRouter } from './vue-router'
 import { vue } from './vue'
-import { supportSetupName } from './vue-support-setup-name'
-import { jsObfuscator } from './obfuscator'
 
 export const plugins = [
+  vueRouter(),
+  vue(),
+  supportSetupName(),
+  vueI18n(),
   autoImports(),
   components(),
   elementPlus(),
   layouts(),
   unocss(),
-  vitePWA(),
   vueDevTools(),
-  vueRouter(),
-  vueI18n(),
-  vue(),
-  supportSetupName(),
-  jsObfuscator(),
+  vitePWA(),
 ]
--- plugins/web-font-download.ts
+++
@@ -1,6 +0,0 @@
-import WebfontDownload from 'vite-plugin-webfont-dl'
-
-// https://github.com/feat-agency/vite-plugin-webfont-dl
-export function webFontDownload() {
-  return WebfontDownload()
-}
