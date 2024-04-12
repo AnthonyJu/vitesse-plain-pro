3c8b33eb96a1db8815e42a43df8eff6eae78a3e8
AnthonyJu <1662345805@qq.com>
2024-04-12 22:50:10 +0800

    Update Emitter and uno.config.ts files

--- uno.config.ts
+++ uno.config.ts
@@ -20,17 +20,23 @@ export default defineConfig({
     'bg-default': 'bg-$el-layout-bg-color dark:bg-$el-layout-bg-color-dark',
   },
   presets: [
+    // 启用 uno 预设
     presetUno(),
+    // 启用 属性 预设
     presetAttributify(),
+    // 启用 icons 预设
     presetIcons({
       scale: 1.2,
       warn: true,
       cdn: 'https://esm.sh/',
     }),
+    // 启用 排版 预设
     presetTypography(),
   ],
   transformers: [
+    // 启用 @apply 功能
     transformerDirectives(),
+    // 启用 () 分组功能，hover:(bg-blue-500 font-large) font-(dark 900)
     transformerVariantGroup(),
   ],
 })
