2ff55d3002a4873b06eb4c36f47d1beedc48297e
jupeng <1662345805@qq.com>
2024-08-01 14:37:29 +0800

    feat: Add ErrorPage component for displaying error information

---
+++ src/components/ErrorPage/ErrorPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="full flex-center flex-wrap">
+    <div class="h-full min-w-500px flex-col-center animate-head-shake">
+      <div class="mb-15px text-55px text-$el-color-primary">
+        {{ errorInfo.type }}
+      </div>
+
+      <div class="mb-10px text-22px text-$el-text-color-primary">
+        {{ errorInfo.msg }}
+      </div>
+
+      <div class="mb-30px text-12px text-$el-text-color-secondary">
+        {{ errorInfo.subMsg }}
+      </div>
+
+      <div>
+        <el-button type="primary" round @click="$router.back()">
+          返回上一页
+        </el-button>
+        <el-button
+          v-if="errorInfo.type === 401"
+          type="warning"
+          round
+          @click="userStore.handleLogout()"
+        >
+          重新授权
+        </el-button>
+      </div>
+    </div>
+
+    <div class="ml-40px animate-head-shake">
+      <img class="full turn-light dark:turn-dark" :src="errorInfo.img">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  errorInfo: {
+    type: number
+    msg: string
+    subMsg: string
+    img: string
+  }
+}>()
+
+const userStore = useUserStore()
+</script>
--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,49 +1,14 @@
 <template>
-  <div full flex-center flex-wrap>
-    <div h-full min-w-500px flex-col-center>
-      <div
-        class="text-$el-color-primary"
-        mb-15px animate-head-shake text-56px
-      >
-        404
-      </div>
-
-      <div
-        class="text-$el-text-color-primary"
-        mb-10px animate-head-shake text-22px
-      >
-        地址输入错误，请重新输入地址~
-      </div>
-
-      <div
-        class="text-$el-text-color-secondary"
-        mb-30px animate-head-shake text-12px
-      >
-        您可以先检查网址，然后重新输入或给管理员反馈问题。
-      </div>
-
-      <div animate-head-shake>
-        <el-button
-          type="primary"
-          round
-          @click="router.replace('/')"
-        >
-          返回首页
-        </el-button>
-      </div>
-    </div>
-
-    <div ml-40px animate-head-shake>
-      <img class="full turn-light dark:turn-dark" src="@/assets/404.png">
-    </div>
-  </div>
+  <ErrorPage :error-info="errorInfo" />
 </template>
 
-<route lang="yaml">
-meta:
-  layout: none
-</route>
-
 <script setup lang="ts">
-const router = useRouter()
+import img_404 from '@/assets/404.png'
+
+const errorInfo = {
+  type: 404,
+  msg: '网址输入错误，请重新输入网址~',
+  subMsg: '您可以先检查网址，然后重新输入或给管理员反馈。',
+  img: img_404,
+}
 </script>
