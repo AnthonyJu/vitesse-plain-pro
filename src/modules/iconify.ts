593af75579529b06a96808beeaca6f2df71b5345
AnthonyJu <1662345805@qq.com>
2024-09-25 17:10:06 +0800

    feat: 扩展 Vue 组件属性，添加 Iconify 组件支持

--- src/modules/iconify.ts
+++ src/modules/iconify.ts
@@ -1,5 +1,11 @@
 import type { App } from 'vue'
 import { Icon } from '@iconify/vue'
 
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    Iconify: typeof Icon
+  }
+}
+
 // https://iconify.design/
 export default (app: App) => app.component('Iconify', Icon)
