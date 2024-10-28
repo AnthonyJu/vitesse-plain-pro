0913bd6757b523e399b77c7c23f3672c11f950cd
AnthonyJu <1662345805@qq.com>
2024-10-28 15:45:20 +0800

    feat: 根据编辑器环境打开文件(code、webstorm)

--- config/plugins/vue-devtools.ts
+++ config/plugins/vue-devtools.ts
@@ -1,6 +1,8 @@
+import process from 'node:process'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://devtools-next.vuejs.org/guide/vite-plugin
 export function vueDevTools() {
-  return VueDevTools()
+  const launchEditor = process.env.VSCODE_GIT_ASKPASS_NODE ? 'code' : 'webstorm'
+  return VueDevTools({ launchEditor })
 }
