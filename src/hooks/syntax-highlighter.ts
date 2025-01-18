fcee8b53bdf5c08ffb988658654bf2db2127466b
AnthonyJu <1662345805@qq.com>
2025-01-18 17:53:58 +0800

    feat: 更新语法高亮钩子，支持多主题和代码选项

--- src/hooks/syntax-highlighter.ts
+++ src/hooks/syntax-highlighter.ts
@@ -1,36 +1,53 @@
-import type { BundledTheme } from 'shikiji'
+import type { BundledTheme, CodeToHastOptions } from 'shiki'
 import type { ShallowRef } from 'vue'
-import { codeToHtml } from 'shikiji'
+import { codeToHtml } from 'shiki'
 
 /**
- * @description: Syntax highlighter
- * @param {string} str code string
- * @param {string} lang language
- * @param {string|Ref<HTMLElement | undefined>} container container selector
- * @param {BundledTheme} theme theme，default is auto according to system theme（vitesse-dark/vitesse-light）
- * @return { html: Ref<string> | string  } html: highlight HTML string
+ * @description: 语法高亮
+ * @param {string} str 代码字符串
+ * @param {string} lang 语言
+ * @param {string|Ref<HTMLElement | undefined>} container 容器
+ * @param {BundledTheme | [BundledTheme, BundledTheme]} theme 主题，默认为 'vitesse-dark' | 'vitesse-light' 并根据系统主题变化
+ * @param {CodeToHastOptions} codeToHtmlOptions codeToHtml选项
+ * @return { html: Ref<string> | string  } html代码字符串
  */
 export function useSyntaxHighlighter(
   str: string,
   lang: string,
   container?: ShallowRef<HTMLElement | null>,
-  theme?: BundledTheme,
+  theme?: BundledTheme | [BundledTheme, BundledTheme],
+  codeToHtmlOptions?: CodeToHastOptions,
 ): { html: Ref<string> | string } {
+  // 生成的html代码
   const html = ref('')
-  const syntaxTheme = computed(() => theme || (isDark.value ? 'vitesse-dark' : 'vitesse-light'))
 
+  // 默认主题
+  theme = theme || ['vitesse-dark', 'vitesse-light']
+
+  // 语法主题
+  const syntaxTheme = computed(() => {
+    if (typeof theme === 'string') return theme
+    return theme[isDark.value ? 0 : 1]
+  })
+
+  // 监听主题变化
   const unWatch = watch(
-    syntaxTheme,
-    (val) => {
+    isDark,
+    () => {
       codeToHtml(str, {
         lang,
-        theme: val,
+        theme: syntaxTheme.value,
+        ...codeToHtmlOptions,
       })
         .then((value) => {
+          // 设置html代码
           html.value = value
+
+          // 容器存在，设置内容
           if (container) container.value!.innerHTML = value
+
           // 指定了主题，不再根据系统主题变化
-          if (theme) unWatch()
+          if (typeof theme === 'string') unWatch()
         })
     },
     {
